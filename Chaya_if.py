fd1d6405446f97028f100399eb4b4397d8a342d5
Chaya <pqow0708@gmail.com>
2025-10-15 15:11:59 +0800

    if.py

---
+++ Chaya_if.py
@@ -0,0 +1,14 @@
+#
+# Chaya, 2025/10/15
+# file: Chaya_if.py
+# Short description of the task
+#
+
+# 1. Input
+temperature = 30 
+
+# 2. Process
+if temperature > 25: 
+    print("It's too hot outside. Study hard in the library")
+
+# 3. Output
