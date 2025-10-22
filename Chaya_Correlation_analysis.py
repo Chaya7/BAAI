413bb81b9ebabf0dbc775551327f7b8eaf3d1500
Chaya <pqow0708@gmail.com>
2025-10-22 16:38:12 +0800

    correlation

--- Chaya_Correlation_analysis.py
+++ Chaya_Correlation_analysis.py
@@ -5,14 +5,19 @@
 #
 
 import pandas as pd
+from scipy import stats
 
 # 1. Input
-df = pd.read_csv('simple_data.csv')
+df = pd.read_csv('Correlation_Analysis_Data.csv')
+correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue'])
+
 
 # 2. Process
-print(df.isnull().sum())
-print(df.isnull().sum().sum())
+# print(df.isnull().sum())
+# print(df.isnull().sum().sum())
 
 # # 3. Output
 # print("Data loaded successfully!")
 # print(f"Dataset shape:{df.shape}")
+print(f'Correlation:{correlation:.2f}')
+print(f'Pvalue:{pvalue}')
