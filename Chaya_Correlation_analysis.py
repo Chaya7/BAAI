aa1672e49d4a7bfa13338cf42f21fd3a65511726
Chaya <pqow0708@gmail.com>
2025-10-22 16:46:58 +0800

    iloc code

--- Chaya_Correlation_analysis.py
+++ Chaya_Correlation_analysis.py
@@ -5,19 +5,26 @@
 #
 
 import pandas as pd
+import numpy as np
 from scipy import stats
 
 # 1. Input
 df = pd.read_csv('Correlation_Analysis_Data.csv')
-correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue'])
 
+df.info()
+
+
+# correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue'])
 
 # 2. Process
 # print(df.isnull().sum())
 # print(df.isnull().sum().sum())
+correlation_matrix=df.iloc[:,1:6]. corr()
+
+print(correlation_matrix)
 
 # # 3. Output
 # print("Data loaded successfully!")
 # print(f"Dataset shape:{df.shape}")
-print(f'Correlation:{correlation:.2f}')
-print(f'Pvalue:{pvalue}')
+# print(f'Correlation:{correlation:.2f}')
+# print(f'Pvalue:{pvalue}')
