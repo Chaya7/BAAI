2fb76f2924af3c916bb2ecb383acb363be2d365d
Chaya <pqow0708@gmail.com>
2025-10-22 15:21:43 +0800

    pd

--- Chaya_Correlation_analysis.py
+++ Chaya_Correlation_analysis.py
@@ -4,8 +4,13 @@
 # Short description of the task
 #
 
+import pandas as pd
+
 # 1. Input
+df = pd.read_csv('Correlation_Analysis_Data.csv')
 
 # 2. Process
 
 # 3. Output
+print("print data loaded successfully!")
+print(f"Dataset shape:{df.shape}")
