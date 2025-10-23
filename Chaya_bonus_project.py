b2c6100c191872271f9e7a3112011aede73dc9e5
Chaya <pqow0708@gmail.com>
2025-10-23 15:29:32 +0800

    bonus step one

---
+++ Chaya_bonus_project.py
@@ -0,0 +1,18 @@
+#
+# mini, 2025/10/23
+# File: mini_project.py
+# Short description of the task
+#
+
+# 1. Input
+datas = [
+    ["Employee_Name |" "Monthly_Sales |" "Sales_Target"], 
+    ["John Smith|" "45000 |" "40000" ],
+    ["Sarah Lee |" "38000 |" "40000" ],
+    ['Mike Chen |' '52000 |' '40000'],
+    ['Anna Wong |' '41000 |' '40000']  ] 
+
+print("SALES PERFORMANCE REPORT\n" "========================\n")
+# 2. Process
+
+# 3. Output
---
+++ test.py
@@ -0,0 +1,19 @@
+{
+    "Task Template": {
+        "prefix": "chayatask",
+        "body": [
+            "#",
+            "# ${TM_FILENAME_BASE/(^[^_]*)_.*/$1/}, ${CURRENT_YEAR}/${CURRENT_MONTH}/${CURRENT_DATE}",
+            "# File: ${TM_FILENAME}",
+            "# Short description of the task",
+            "#",
+            "",
+            "# 1. Input",
+            "",
+            "# 2. Process",
+            "",
+            "# 3. Output"
+        ],
+    "description": "Insert chaya' task template"
+    }
+}
