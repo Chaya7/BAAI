c102cf0607e235b3c5af7456b3e649b65e9b59f1
Chaya <pqow0708@gmail.com>
2025-10-23 15:46:29 +0800

    change

--- Chaya_bonus_project.py
+++ Chaya_bonus_project.py
@@ -4,15 +4,14 @@
 # Short description of the task
 #
 
-# 1. Input
-datas = [
-    ["Employee_Name |" "Monthly_Sales |" "Sales_Target"], 
-    ["John Smith|" "45000 |" "40000" ],
-    ["Sarah Lee |" "38000 |" "40000" ],
-    ['Mike Chen |' '52000 |' '40000'],
-    ['Anna Wong |' '41000 |' '40000']  ] 
+import pandas as pd
+df = pd.read_excel('sales_data.xlsx')
 
-print("SALES PERFORMANCE REPORT\n" "========================\n")
+print(df)
+# 1. Input (provide the dataset) 
+
+
 # 2. Process
+
 
 # 3. Output
